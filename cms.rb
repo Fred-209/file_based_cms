1ec7f0a6df465a810679a1170d140a11bc62a094
Fred Durham <f.durham@protonmail.com>
2022-06-14 20:22:11 -0500

    added favico.icon and main.css styling

--- cms.rb
+++ cms.rb
@@ -17,7 +17,7 @@ def load_file_content(path)
 
   case File.extname(path)
   when '.md' 
-    render_markdown(content)
+    erb render_markdown(content)
   when '.txt'
     headers['Content-Type'] = 'text/plain'
     content
@@ -30,19 +30,22 @@ def render_markdown(text)
   markdown.render(text)
 end
 
-
-
-
-
-before do 
-  @files = Dir.glob(root + "/data/*").map do |file| 
-    File.basename(file)
+def data_path
+  if ENV['RACK_ENV'] == 'test'
+    File.expand_path("../tests/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
   end
 end
 
+
 # Index page
 get "/" do 
-    
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path| 
+    File.basename(path)
+  end
+
   erb :index, layout: :layout
 end
 
@@ -50,7 +53,7 @@ end
 get "/:filename" do 
 
   @file_name = params[:filename]
-  file_path = root + "/data/" + @file_name
+  file_path = File.join(data_path, @file_name)
 
   if File.file?(file_path)
     load_file_content(file_path)
@@ -63,7 +66,7 @@ end
  # Edit a file
 get "/:filename/edit" do
   @file_name = params[:filename]
-  @file_path = root + "/data/" + @file_name
+  @file_path = File.join(data_path, @file_name)
   @content =File.read(@file_path)
   
 
@@ -71,7 +74,7 @@ get "/:filename/edit" do
 end
 
 post "/:filename" do 
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, + params[:filename])
   File.write(file_path, params[:content])
 
   session[:message] = "#{params[:filename]} has been updated."
--- tests/cms_app_tests.rb
+++ tests/cms_app_tests.rb
@@ -3,6 +3,7 @@ ENV["RACK_ENV"] = "test"
 
 require "minitest/autorun"
 require "rack/test"
+require "fileutils"
 
 require_relative "../cms"
 
@@ -13,7 +14,26 @@ class CmsAppTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
   def test_index
+
+    create_document("history.txt")
+    create_document("changes.txt")
+    create_document("about.md")
+
     get "/"
     response_body = last_response.body
 
@@ -25,6 +45,8 @@ class CmsAppTest < Minitest::Test
   end
 
   def test_page_content
+    create_document("history.txt", "2003 - Ruby 1.8 released.")
+
     get "/history.txt"
     body = last_response.body
 
@@ -49,6 +71,8 @@ class CmsAppTest < Minitest::Test
   end
 
   def test_render_markdown
+    create_document("about.md", "# Ruby is...")
+
     get "/about.md"
 
     assert_equal 200, last_response.status
@@ -57,16 +81,17 @@ class CmsAppTest < Minitest::Test
   end
 
   def test_edit_document
+    create_document("history.txt", "<textarea")
+
     get "/history.txt/edit"
 
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<textarea"
-    assert_includes last_response.body, 'Edit contents of history.txt:'
-    
-
   end
 
   def test_update_document
+    create_document("changes.txt")
+
     post "/changes.txt", content: "new content"
 
     assert_equal 302, last_response.status
